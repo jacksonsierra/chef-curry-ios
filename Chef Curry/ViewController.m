9ac55f204f9141b60c55ccfda0fe224f6788d59a
Jackson Sierra <jks718@gmail.com>
2016-03-22 14:01:15 -0700

    [feature] (ViewController.h) add controller logic for rendering UI assets to View

--- Chef Curry/ViewController.m
+++ Chef Curry/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "UIAssets.h"
+
+@import CoreGraphics;
 
 @interface ViewController ()
 
@@ -16,12 +19,78 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  UIAssets *assets = [[UIAssets alloc] init];
+  assetsLibrary = assets.library;
+  
+  if (!_status) {
+    
+  }
+  [self updateUI];
 }
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+  return UIStatusBarStyleLightContent;
+}
+
+- (void)updateUI {
+  statusAssets = [assetsLibrary valueForKey:self.status];
+  
+  [_statusImage setImage:(UIImage *)[self findStatusImage]];
+  [_statusImage setContentMode:UIViewContentModeScaleAspectFit];
+  [_statusLabel setText:(NSString *)[self findStatusLabel]];
+  [_statusLabel setFont:(UIFont *)[self findStatusLabelFont]];
+  [_statusText setText:(NSString *)[self findStatusText]];
+//  [_statusLabel setTextColor:(UIColor *)[self findStatusLabelColor]];
+}
+
+- (UIImage *)findStatusImage {
+  NSArray *images = [statusAssets valueForKey:kImages];
+  NSString *imageName;
+  int randomNum = arc4random_uniform((unsigned int)images.count);
+  
+  imageName = images[randomNum];
+  
+  if (!imageName) {
+    return nil;
+  }
+  
+  return [UIImage imageNamed:imageName];
+}
+
+- (NSString *)findStatusLabel {
+  return [statusAssets valueForKey:kLabel];
+}
+
+- (NSString *)findStatusText {
+  return [statusAssets valueForKey:kText];
+}
+
+- (UIFont *)findStatusLabelFont {
+  NSDictionary *font = [statusAssets valueForKey:kFont];
+  NSString *fontName = [font valueForKey:kFontName];
+  CGFloat fontSize = [[font valueForKey:kFontSize] doubleValue];
+  
+  if (!(fontName || fontSize)) {
+    return nil;
+  }
+  
+  return [UIFont fontWithName:fontName size:fontSize];
+}
+
+- (UIColor *)findStatusLabelColor {
+  NSDictionary *statusColor = [statusAssets valueForKey:kLabelColor];
+
+  CGFloat red = [[statusColor valueForKey:@"red"] doubleValue]/255;
+  CGFloat green = [[statusColor valueForKey:@"green"] doubleValue]/255;
+  CGFloat blue = [[statusColor valueForKey:@"blue"] doubleValue]/255;
+  CGFloat alpha = [[statusColor valueForKey:@"alpha"] doubleValue]/255;
+  
+  return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
 
 @end
