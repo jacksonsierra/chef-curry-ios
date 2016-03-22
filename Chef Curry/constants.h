31241a4469aca9abb624e6c6d3eaced218548471
Jackson Sierra <jks718@gmail.com>
2016-03-22 14:02:38 -0700

    [feature] (constants.h) add constants file containing Server url

---
+++ Chef Curry/constants.h
@@ -0,0 +1,14 @@
+//
+//  constants.h
+//  Chef Curry
+//
+//  Created by Jackson Sierra on 3/21/16.
+//  Copyright © 2016 Jackson Sierra. All rights reserved.
+//
+
+#ifndef constants_h
+#define constants_h
+
+#define ServerApiURL @"http://159.203.244.50:5000"
+
+#endif /* constants_h */
